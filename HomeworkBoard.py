68b93fca727eca4971ffd94b1d6f5a2b49bc8951
carrot729 <hnf729@outlook.com>
2024-09-24 21:38:13 +0800

    1234

--- HomeworkBoard.py
+++ HomeworkBoard.py
@@ -1,22 +1,26 @@
-import os, datetime
+import os, datetime,time,json
 
-class HomeworkBoard(object):
-    def __init__(self, backup_dir='.\\backup\\',
-                 template_name='template.docx',
-                 homework='作业.docx',
-                 desktop_path=os.environ.get('USERPROFILE')+'\\Desktop\\'):
-        self.welcome()
-        self.backup_dir = backup_dir
-        self.template_name = template_name
-        self.homework = homework
-        self.desktop_path = desktop_path
-        if self.today_used():
-            if input('WARNING:today_used,do you want to OVERWRITE this?(y/N):') != 'y':
-                exit(0)
+def output(word: str = '', end: str = '') -> str:
+    """
+    make SPECIAL output!!!
+    :param word: the word you say
+    :param end: the end part
+    :return: word
+    """
+    for i in word:
+        time.sleep(0.01)
+        print(i,end='')
+    print(end)
+    return word
 
-    @staticmethod
-    def welcome():
-        print('''
+DEFAULT_SETTING = {
+    "template_path":".\\template.docx",
+    "homework_path":"{0}\\Desktop\\homework.docx".format(os.environ['USERPROFILE']),
+    "backup_path":".\\backup"
+}
+SETTING_FILE_NAME = 'HomeworkBoard.setting.caca.json'
+
+print('''
     *************************************
      ######     ###     ######     ###    
     ##    ##   ## ##   ##    ##   ## ##   
@@ -27,37 +31,37 @@ class HomeworkBoard(object):
      ######  ##     ##  ######  ##     ## 
     *************************************
             ''')
-        print('Welcome to HomeworkBoard.')
-    @staticmethod
-    def success():
-        print("Done!!!")
-    def today_used(self):
-        try:
-            open(self.backup_dir + str(datetime.date.today()) + '.docx')
-        except IOError:
-            return False
-        else:
-            return True
-    def make_backup(self):
-        print('Creating backup...')
-        try:
-            homework=open(self.desktop_path +self.homework, 'rb')
-            backup_file = open(self.backup_dir + str(datetime.date.today()) + '.docx', 'wb')
-            backup_file.write(homework.read())
-            homework.close()
-            backup_file.close()
-        except Exception as e:
-            print(f'Error: {str(e)}')
-            exit(1)
-    def make_homework(self):
-        print('Creating homework...')
-        try:
-            homework=open(self.desktop_path +self.homework, 'wb')
-            template_file = open(self.template_name, 'rb')
-            homework.write(template_file.read())
-            homework.close()
-            template_file.close()
-        except Exception as e:
-            print(f'Error: {str(e)}')
-            exit(1)
+output('Welcome to HomeworkBoard.')
+
+def read_setting() -> dict:
+    """
+    Read setting file
+    :return: setting
+    """
+    with open(SETTING_FILE_NAME, "r") as f:
+        setting = json.loads(f.read())
+
+    return setting
+
+def write_setting(setting:dict) -> None:
+    """
+    Read setting file
+    :param setting: dict
+    :return:
+    """
+    with open(SETTING_FILE_NAME, "r") as f:
+        f.write(json.dumps(setting, sort_keys=True, indent=4, separators=(',', ': ')))
+def make_backup(homework:str,backup:str) -> bool:
+    """
+    Make backup file.
+    :param homework: homework file
+    :param backup: Backup file
+    :return: success
+    """
+    success=0
+    try:
+        homework_file=open(homework,'rb')
+        backup_file=open(backup,'wb')
+        backup_file.write(homework_file.read())
+    except :
 
--- main.py
+++
@@ -1,9 +0,0 @@
-from HomeworkBoard import HomeworkBoard
-import os
-
-homework_board = HomeworkBoard()
-
-if __name__=='__main__':
-    homework_board.make_backup()
-    homework_board.make_homework()
-    os.system("Pause")
